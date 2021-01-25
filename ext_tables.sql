6dc8bed8c3fedec6e0b6c361e684f593fc66aa00
Markus Klein <markus.klein@reelworx.at>
2021-01-25 16:05:33 +0100

    [BUGFIX] Add missing DB field for campaign to ext_tables.sql
    Resolves: #9

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,6 @@
-CREATE TABLE tt_content (
-    skilldisplay_skills TEXT,
-    skilldisplay_skillset int(11) unsigned DEFAULT '0' NOT NULL
+CREATE TABLE tt_content
+(
+    skilldisplay_skills   TEXT,
+    skilldisplay_skillset int(11) unsigned DEFAULT '0' NOT NULL,
+    skilldisplay_campaign varchar(200)     DEFAULT ''  NOT NULL
 );
