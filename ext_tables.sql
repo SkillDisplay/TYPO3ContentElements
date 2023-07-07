573e1baedd0b4ad0f8f19f0516fbff3d02ebe6cc
Markus Klein <markus.klein@reelworx.at>
2023-07-07 09:04:40 +0200

    [BUGFIX] Allow multi-value skillset selection

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tt_content
 (
-    skilldisplay_skills   TEXT,
-    skilldisplay_skillset int(11) unsigned DEFAULT '0' NOT NULL,
-    skilldisplay_campaign varchar(200)     DEFAULT ''  NOT NULL
+    skilldisplay_skills   varchar(200) DEFAULT '' NOT NULL,
+    skilldisplay_skillset varchar(200) DEFAULT '' NOT NULL,
+    skilldisplay_campaign varchar(200) DEFAULT '' NOT NULL
 );
